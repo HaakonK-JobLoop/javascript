8dba74c50513a999c5bd08c2a0b2165f9f5a1622
HakonK-JobLoop <haakon.k@jobloop.no>
2024-10-23 21:15:39 +0200

    Jæ

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-// let phrase = "Strings are cool";
+// Working with strings
 
-// document.write(phrase.substring(2, 7));
+// let phrase = "Strings are fun!";
 
-console.log("Hello world!");
+// console.log(phrase.length);
+
+// console.log(phrase.toUpperCase());
+
+// console.log(phrase.toLowerCase());
+
+// console.log(phrase.charAt(5));
+// "ABCDEFG"
+//  0123456
+
+// console.log(phrase.indexOf("e"));
+// console.log(phrase.lastIndexOf("s"));
+
+// console.log(phrase.substring(0, 7));
+// console.log(phrase.substring(phrase.indexOf("f"), phrase.length));
+
+// console.log(phrase.endsWith("fun!"));
+// console.log(phrase.endsWith("funny!"));
+
+// let text = "are fun!";
+// console.log(phrase.includes("Strings"));
+// console.log(phrase.includes(text));
+
+// Working with math and numbers
+
+// Math.abs(num), Math.max(num1, num2), Math.min(num1, num2), Math.round(number)
+// Math.pow(num, power), Math.sqrt(num), Math.random(), Math.floor()
+
+// Se an om vi får tid til å prate om user input og kalkulator
